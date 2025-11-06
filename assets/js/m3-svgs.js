9508d3e39971597d5f06adc420f06009131ce430
Shaine ONeal <shaineoneal@gmail.com>
2025-11-05 19:09:45 -0600

    wrapped functions in if statements to prevent running when not on the correct page

--- assets/js/m3-svgs.js
+++ assets/js/m3-svgs.js
@@ -19,46 +19,44 @@
       }, 3000); // Auto-dismiss after 3 seconds
     }
 
-    document.addEventListener('DOMContentLoaded', function() {
-    console.log('M3 SVGs script loaded.');
-    const shapes = document.querySelectorAll('.shape');
+const shapes = document.querySelectorAll('.shape');
 
-    shapes.forEach(shape => {
-        shape.addEventListener('click', function() {
-
-            // is auto-copy enabled?
-            const autoCopyEnabled = document.body.classList.contains('auto-copy-enabled');
-            if (autoCopyEnabled) {
-                // get SVG code from inner svg
-                const svgElement = this.querySelector('svg');
-                if (svgElement) {
-                    const svgCode = svgElement.outerHTML;
-                    navigator.clipboard.writeText(svgCode);
-                    showNotification('SVG code copied to clipboard!');
-                }
-                // get element id
-                const shape_id = this.classList[1]; // assuming the second class is the id
-                // Make element visible
-                const target = document.getElementById(shape_id);
-                if (target) {
-                    target.classList.remove('hidden');
-                }
-            } else {
-                // get element id
-                const shape_id = this.classList[1]; // assuming the second class is the id
-                // Make element visible
-                const target = document.getElementById(shape_id);
-                if (target) {
-                    target.classList.remove('hidden');
-                    target.scrollIntoView({ behavior: 'smooth', block: 'center' });
-                }
+shapes.forEach(shape => {
+    shape.addEventListener('click', function() {
+        // is auto-copy enabled?
+        const autoCopyEnabled = document.body.classList.contains('auto-copy-enabled');
+        if (autoCopyEnabled) {
+            // get SVG code from inner svg
+            const svgElement = this.querySelector('svg');
+            if (svgElement) {
+                const svgCode = svgElement.outerHTML;
+                navigator.clipboard.writeText(svgCode);
+                showNotification('SVG code copied to clipboard!');
             }
-
-            
-        });
+            // get element id
+            const shape_id = this.classList[1]; // assuming the second class is the id
+            // Make element visible
+            const target = document.getElementById(shape_id);
+            if (target) {
+                target.classList.remove('hidden');
+            }
+        } else {
+            // get element id
+            const shape_id = this.classList[1]; // assuming the second class is the id
+            // Make element visible
+            const target = document.getElementById(shape_id);
+            if (target) {
+                target.classList.remove('hidden');
+                target.scrollIntoView({ behavior: 'smooth', block: 'center' });
+            }
+        }
+        
     });
+});
 
-    const autoCopyCheckbox = document.querySelector('#auto-copy-checkbox');
+const autoCopyCheckbox = document.querySelector('#auto-copy-checkbox');
+if (autoCopyCheckbox) {
+
     autoCopyCheckbox.addEventListener('click', function() {
         console.log('Auto Copy SVG Code checkbox changed:', this.checked);
         if (this.checked) {
@@ -67,8 +65,10 @@
             document.body.classList.remove('auto-copy-enabled');
         }
     });
+}
 
-    const showAllCheckbox = document.getElementById('show-all-checkbox');
+const showAllCheckbox = document.getElementById('show-all-checkbox');
+if (showAllCheckbox) {
     showAllCheckbox.addEventListener('change', function() {
         console.log('Show All Previews checkbox changed:', this.checked);
         const previews = document.querySelectorAll('figure.code-block');
@@ -78,6 +78,4 @@
             previews.forEach(preview => preview.classList.add('hidden'));
         }
     });
-});
-
-
+}
